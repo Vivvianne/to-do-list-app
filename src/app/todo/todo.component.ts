16dee4a1765b81cde4059835e6f7da9a6474855a
Vivvianne <ndiahav@gmail.com>
2020-01-30 18:59:34 +0300

    added onadd o the tdod.component.ts

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers : [TodoService]
 })
 export class TodoComponent implements OnInit {
+  toDoListArray: any[];
 
-  constructor() { }
+  constructor(private toDoService: TodoService) { }
 
   ngOnInit() {
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element => {
+        var x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
+      this.toDoListArray.sort((a,b) => {
+        return a.isChecked - b.isChecked;
+      })
+    });
+  }
+  onAdd(itemTitle) {
+    this.toDoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
   }
 
 }
